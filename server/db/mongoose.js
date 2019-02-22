6f509a85e411b9272675b7975f10f737518b1d0f
Shawn Loh <shawnloh1996@gmail.com>
2019-02-22 20:15:58 +0800

    Added link for mongoose

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
-  useNewUrlParser: true
-});
+
+mongoose.connect(
+  'mongodb://todoapp:BJSva2d8y!F7&CcDM8LD@ds237072.mlab.com:37072/todoapp-456',
+  {
+    useNewUrlParser: true
+  }
+);
 
 module.exports = {
   mongoose
