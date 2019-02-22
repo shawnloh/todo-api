38a7ea50c6737a752a9e2c5eaa4418f3438e84e3
Shawn Loh <shawnloh1996@gmail.com>
2019-02-22 19:15:14 +0800

    Added GET /todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,16 @@ app.post('/todos', (req, res) => {
     .catch(e => res.status(400).send(e));
 });
 
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then(todos => {
+      res.send({ todos });
+    })
+    .catch(error => {
+      res.status(400).send(error);
+    });
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Started on port ${PORT}`);
