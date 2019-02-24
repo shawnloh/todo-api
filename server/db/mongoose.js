d17142d997d520708a7fd99e9f5ce05f5daac2df
Shawn Loh <shawnloh1996@gmail.com>
2019-02-24 13:56:58 +0800

    Added PATCH /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 mongoose.connect(
-  // 'mongodb://todoapp:BJSva2d8y!F7&CcDM8LD@ds237072.mlab.com:37072/todoapp-456',
-  'mongodb://localhost:27017/TodoApp',
+  'mongodb://todoapp:BJSva2d8y!F7&CcDM8LD@ds237072.mlab.com:37072/todoapp-456',
+  // 'mongodb://localhost:27017/TodoApp',
   {
     useNewUrlParser: true
   }
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 const { ObjectID } = require('mongodb');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -68,8 +69,36 @@ app.delete('/todos/:id', (req, res) => {
       return res.send({ todo });
     })
     .catch(e => {
-      console.log(e);
-      res.status(404).send();
+      // console.log(e);
+      res.status(400).send();
+    });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  let body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      return res.send({ todo });
+    })
+    .catch(e => {
+      res.status(400).send();
     });
 });
 
