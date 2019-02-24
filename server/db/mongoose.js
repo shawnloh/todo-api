a09e7846345c8ff996ff051bbc4de14d328c4f5d
Shawn Loh <shawnloh1996@gmail.com>
2019-02-24 13:59:55 +0800

    Modified url for new db link

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,13 +2,9 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(
-  'mongodb://todoapp:BJSva2d8y!F7&CcDM8LD@ds237072.mlab.com:37072/todoapp-456',
-  // 'mongodb://localhost:27017/TodoApp',
-  {
-    useNewUrlParser: true
-  }
-);
+mongoose.connect(process.env.DB || 'mongodb://localhost:27017/TodoApp', {
+  useNewUrlParser: true
+});
 
 module.exports = {
   mongoose
