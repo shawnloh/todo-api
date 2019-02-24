3ad4103bf4dc46e4cc26084a0fcdfc16df133604
Shawn Loh <shawnloh1996@gmail.com>
2019-02-24 19:42:38 +0800

    Add tests for POST /users/login

--- server/server.js
+++ server/server.js
@@ -145,8 +145,7 @@ app.post('/users/login', (req, res) => {
       });
     })
     .catch(e => {
-      console.log(e);
-      res.status(401).send();
+      res.status(400).send();
     });
 });
 
