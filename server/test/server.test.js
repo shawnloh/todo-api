024364ea301b2996abc342a4885e7aff1a095356
Shawn Loh <shawnloh1996@gmail.com>
2019-02-25 16:10:56 +0800

    Added tests for DELETE /users/me/token

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -311,3 +311,29 @@ describe('POST /users/login', () => {
       });
   });
 });
+
+describe('DELETE /users/me/token', () => {
+  it('should remove auth token on logout', done => {
+    // DELETE
+    request(app)
+      .delete('/users/me/token')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+
+        expect(res.header['x-auth']).toBeUndefined();
+        expect(res.body).toEqual({});
+
+        User.findById(users[0]._id)
+          .then(user => {
+            expect(user.tokens.length).toBe(0);
+            done();
+          })
+          .catch(e => done(e));
+      });
+    // SET X-AUTH equal to token
+    // 200back
+    //find user in db, verify that token array has length of zero
+  });
+});
